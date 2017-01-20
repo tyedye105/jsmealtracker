55a3ac235a2c1bd4ec63167ca9c7e9cdc2d4aa19
Marty Kovach <tyedye105@yahoo.com>
2017-01-20 10:21:19 -0800

    displays meal entries from seeded array in app.component, with a loop.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1> Meal Tracker</h1>
-  <p> Welcome to the meal tracker!  Need help trying to keep track of everything eaten today? This this app is for! </p>
-  <br>
-  <h2> Food eaten on {{date}} </h2>
-  <br>
-  <p> Food 1, desc: shouldn't have eaten that, 345 cal, 1-20-17</p>
-  <br>
-  <p> Food 2, desc: worth it, 545 cal, 1-20-17</p>
-  <br>
-  <p> Food 3, desc: I need to learn how to cook..., 745 cal, 1-20-17</p>
-
+  <div class="container">
+    <h1> Meal Tracker</h1>
+    <p> Welcome to the meal tracker!  Need help trying to keep track of everything eaten today? This this app is for! </p>
+    <br>
+    <h2> Food eaten on current date! </h2>
+    <div *ngFor="let currentMeal of masterMealList">
+    <h3>{{currentMeal.name}}</h3>
+    <h4>Details:</h4>
+    <h5>{{currentMeal.details}}</h5>
+    <h4>Calories: {{currentMeal.calories}}</h4>
+    <h5> Eaten on: {{currentMeal.logDate}}</h5>
+    </div>
+  </div>
   `
 })
 
-export class AppComponent {}
+export class AppComponent {
+  masterMealList: Meal[] = [
+    new Meal("apple", "they saw it keeps the dentist away", 100, "1-20-17"),
+    new Meal("pbj", "the classic", 300, "1-20-17"),
+    new Meal("chips", "I couldn't deny the crunchy goodness ", 390, "1-20-17")
+  ]
+}
